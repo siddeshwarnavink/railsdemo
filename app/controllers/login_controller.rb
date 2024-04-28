a38ecc565ee903ce1ac09c30742e5f45a084cba5
Harish0311 <harishgamer00@gmail.com>
2024-04-28 15:59:24 +0530

    it's funny i decided to login without password. i realized my mistake end i fixed it
    --- THE END ---

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -4,16 +4,14 @@ class LoginController < ApplicationController
 
   def create
     @user = User.find_by(email: params[:user][:email].downcase)
-    if @user.present?
-        login @user
-        redirect_to root_path, notice: "Signed in."
-      else
-        flash.now[:alert] = "Incorrect email or password."
-        render :new, status: :unprocessable_entity
-      end
-
+    if @user.present? && @user.authenticate(params[:user][:password])
+      login @user
+      redirect_to root_path, notice: "Signed in."
+    else
+      flash.now[:alert] = "Incorrect email or password."
+      render :new, status: :unprocessable_entity
     end
-
+  end
 
   def destroy
     logout
