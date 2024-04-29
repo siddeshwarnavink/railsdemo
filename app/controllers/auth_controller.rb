b6a54bef92dea6238cc6a9d6b3273bd663a0ad92
Harish0311 <harishgamer00@gmail.com>
2024-04-29 09:39:59 +0530

    made some refactoring changes

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,37 @@
+class AuthController < ApplicationController
+  include Authentication
+  before_action :redirect_if_authenticated, only: [:get_login, :post_login]
+
+  def get_login
+    @user = User.new
+  end
+
+  def post_login
+    @user = User.find_by(email: params[:user][:email].downcase)
+    if @user.present? && @user.authenticate(params[:user][:password])
+      login @user
+      redirect_to root_path, notice: "Signed in."
+    else
+      flash.now[:alert] = "Incorrect email or password."
+      render :get_signup, status: :unprocessable_entity
+    end
+  end
+
+  def post_logout
+    logout
+    redirect_to root_path, notice: "Signed out."
+  end
+
+  def get_signup
+    @user = User.new
+  end
+
+  def post_signup
+    @user = User.new(params.require(:user).permit(:email, :password, :password_confirmation))
+    if @user.save
+      redirect_to login_path, notice: "Account created! Please login now"
+    else
+      render :get_signup, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   get "/auth/signup", to: "auth#get_signup", as: "signup"
   post "/auth/signup", to: "auth#post_signup"
-  get "/auth/login", to: "auth#get_login", as: "login"
+  get "/auth/login", to: "auth#get_login" , as: "login"
+  post "/auth/login", to: "auth#post_login"
   post "/auth/logout", to: "auth#post_logout"
 
   get '/admin/create', to: "admin#create", as: "create_product"
